2eb7e5f456aa29cda9282e84b0308d608bc04424
Andy Hausmann <hi@andy-hausmann.de>
2014-07-21 01:29:45 +0200

    Removed unnecessary dependency.

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,6 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'typo3' => '6.1.0-6.2.99',
 			'cms' => '',
-			'fluidcontent' => '',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
