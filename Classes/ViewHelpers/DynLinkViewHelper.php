ab75a84ee0cf74ee32df571c8d07228fa5cfdbd3
Stefano Hämmerle <stefano.haemmerle@gbd.at>
2017-03-16 14:17:09 +0100

    //WIP

--- Classes/ViewHelpers/DynLinkViewHelper.php
+++ Classes/ViewHelpers/DynLinkViewHelper.php
@@ -93,7 +93,7 @@ class DynLinkViewHelper extends AbstractTagBasedViewHelper {
 			// Save link data into ViewHelper arguments
 			$this->setArgumentsFromArray($paramDataArr);
 
-			$cObj = GeneralUtility::makeInstance('\TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer');
+			$cObj = GeneralUtility::makeInstance('TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer');
 			$configuration = array(
 				'parameter' => $this->arguments['href'],
 				'returnLast' => TRUE
